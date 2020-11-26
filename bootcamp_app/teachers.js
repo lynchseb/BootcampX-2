c1020399cc13ba4e58f83d4babb63832455786f1
Sebastian <lynchseb@gmail.com>
2020-11-26 16:24:51 +0000

    add injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,22 +8,27 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query (
-  `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(*) as total_assistances
-  FROM assistance_requests
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-  GROUP BY teachers.name, cohorts.name
-  ORDER BY teacher;
-  `
-)
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(*) as total_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE 1$
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+`
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.log('query error', err.stack));
 
 // pool.query(`
 // SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
